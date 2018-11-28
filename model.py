c87d5547b3a75b7677e908606808f00161d4a8ce
uta <utashih@gmail.com>
2018-11-28 14:23:25 -0800

    add PlacesCNN

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import torch
 import torchvision.models as models
 import torch.nn as nn
+import os 
 
 class VGG16_fc7_object(nn.Module):
     def __init__(self):
@@ -12,3 +13,22 @@ class VGG16_fc7_object(nn.Module):
 
     def forward(self, x):
         return self.fc7(x)
+
+class PlacesCNN(nn.Module):
+    def __init__(self, arch='resnet50'):
+        super(PlacesCNN, self).__init__()
+
+        model_file = '%s_places365.pth.tar' % arch
+        if not os.access(model_file, os.W_OK):
+            weight_url = 'http://places2.csail.mit.edu/models_places365/' + model_file
+            os.system('wget ' + weight_url)
+        model = models.__dict__[arch](num_classes=365)
+        checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+        state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+        model.load_state_dict(state_dict)
+
+        layers = list(model.children())[:-1]
+        self.backbone = nn.Sequential(*layers)
+        
+    def forward(self, x):
+        return self.backbone(x)
