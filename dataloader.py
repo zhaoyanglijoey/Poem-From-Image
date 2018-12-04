ea8c5f594de13712764da04b651d6c56b9d5f292
Zhaoyang Li <zhaoyang_li@sfu.ca>
2018-12-04 13:47:18 -0800

    add bert generate; not working

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,37 @@
 import torch
-from torch.utils.data import Dataset, TensorDataset
+from torch.utils.data import Dataset, TensorDataset, DataLoader
 from PIL import Image
 import os, random, sys
 import util
+from tqdm import tqdm
 
 def convert_to_bert_ids(seq, tokenizer, max_seq_len):
     tokens = tokenizer.tokenize(seq)
     if len(tokens) > max_seq_len - 2:
         tokens = tokens[0:(max_seq_len-2)]
-
+    # length = len(tokens)
     tokens.insert(0, '[CLS]')
     tokens.append('[SEP]')
+    ids = tokenizer.convert_tokens_to_ids(tokens)
+    padded_ids = [0] * max_seq_len
+    padded_ids[:len(ids)] = ids
+    mask = [0] * max_seq_len
+    mask[:len(ids)] = [1] * len(ids)
+
+    # assert len(padded_ids) == max_seq_len
+    # assert len(mask) == max_seq_len
+
+    padded_ids = torch.tensor(padded_ids, dtype=torch.long)
+    mask = torch.tensor(mask, dtype=torch.long)
+
+    return padded_ids, mask
+
+def convert_to_bert_ids_no_sep(seq, tokenizer, max_seq_len):
+    tokens = tokenizer.tokenize(seq)
+    if len(tokens) > max_seq_len - 2:
+        tokens = tokens[0:(max_seq_len-2)]
+    # length = len(tokens)
+    tokens.insert(0, '[CLS]')
     ids = tokenizer.convert_tokens_to_ids(tokens)
     padded_ids = [0] * max_seq_len
     padded_ids[:len(ids)] = ids
@@ -76,46 +97,85 @@ def aligned_ids(seq, basic_tokenizer, tokenizer, word2idx, max_seq_len):
     basic_tokens.insert(0, '[CLS]')
     align_mask = [0] * max_seq_len
 
-    word_ind = []
+    word_ind = [0] * max_seq_len
     i = 0
     for j, token in enumerate(tokens):
         if token.startswith('##'):
             continue
         else:
             if token=='[SEP]':
-                word_ind.append(word2idx['[SEP]'])
-                align_mask[j] = 1
+                word_ind[i] = (word2idx['[SEP]'])
+                i += 1
+                # align_mask[j] = 1
                 break
+            if not basic_tokens[i].startswith(token):
+                print(basic_tokens, tokens, basic_tokens[i], token)
             assert basic_tokens[i].startswith(token)
-            word_ind.append(word2idx[basic_tokens[i]])
+            word_ind[i] = word2idx[basic_tokens[i]]
             align_mask[j] = 1
             i += 1
+    length_m1 = torch.tensor(i-1, dtype=torch.long)
     align_mask = torch.tensor(align_mask, dtype=torch.long)
     word_ind = torch.tensor(word_ind, dtype=torch.long)
 
-    return padded_ids, attention_mask, align_mask, word_ind
+    return padded_ids, attention_mask, align_mask, word_ind, length_m1
 
 
 
 def build_unim_dataset(data, features, basic_tokenizer, tokenizer, word2idx, max_seq_len=256):
     id_list = []
-    mask_list = []
+    attn_mask_list = []
+    align_mask_list = []
+    word_ind_list = []
+    length_m1_list = []
     feature_list = []
     dataloader = []
-    for entry in data:
-        id, attn_mask, align_mask, word_ind = aligned_ids(
+    sys.stderr.write('Building dataset...\n')
+    for entry in tqdm(data):
+        if entry['id'] == 28886:
+            continue
+        id, attn_mask, align_mask, word_ind, length_m1 = aligned_ids(
             entry['poem'], basic_tokenizer, tokenizer, word2idx, max_seq_len)
-        # feature = features[entry['id']]
-        # feature_list.append(feature)
-        # id_list.append(id)
-        # mask_list.append(attn_mask)
-        dataloader.append((id, attn_mask, align_mask, word_ind))
-    # ids = torch.stack(id_list, 0)
-    # masks = torch.stack(mask_list, 0)
-    # feature_tensors = torch.tensor(feature_list)
-    # dataset = TensorDataset(ids, masks, feature_tensors)
+        feature = features[entry['id']]
+        feature_list.append(feature)
+        id_list.append(id)
+        attn_mask_list.append(attn_mask)
+        align_mask_list.append(align_mask)
+        word_ind_list.append(word_ind)
+        length_m1_list.append(length_m1)
+        # dataloader.append((id, attn_mask, align_mask, word_ind))
+    ids = torch.stack(id_list, 0)
+    attn_masks = torch.stack(attn_mask_list, 0)
+    align_masks = torch.stack(align_mask_list, 0)
+    word_inds = torch.stack(word_ind_list, 0)
+    lengths_m1 = torch.stack(length_m1_list, 0)
+    feature_tensors = torch.tensor(feature_list)
+    dataset = TensorDataset(ids, attn_masks, align_masks, word_inds, lengths_m1, feature_tensors)
 
-    return dataloader
+    return dataset
+
+class UnimDataset(Dataset):
+    def __init__(self, data, features, basic_tokenizer, tokenizer, word2idx, max_seq_len):
+        super(UnimDataset, self).__init__()
+        self.data = data
+        self.features = features
+        self.basic_tokenizer = basic_tokenizer
+        self.tokenizer = tokenizer
+        self.word2idx = word2idx
+        self.max_seq_len = max_seq_len
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        entry = self.data[index]
+        if entry['id'] == 28886:
+            return self.__getitem__(random.randrange(len(self.data)))
+        feature = self.features[entry['id']]
+        id, attn_mask, align_mask, word_ind, length_m1 = aligned_ids(
+            entry['poem'], self.basic_tokenizer, self.tokenizer, self.word2idx, self.max_seq_len)
+        return id, attn_mask, align_mask, word_ind, length_m1, feature
+
 
 class PoemImageEmbedDataset(Dataset):
     def __init__(self, data, img_dir, tokenizer, max_seq_len, transform = None):
